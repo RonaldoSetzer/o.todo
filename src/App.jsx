7997649be2cd1bd775b754b363bcf69cb35f89d2
Ronaldo Santiago <ronaldo.setzer@gmail.com>
2020-09-07 22:26:56 -0300

    feat: Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import GlobalStyles from '@/styles/global';
+
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <h1>Todo Todo</h1>
+    </>
+  );
+}
+
+export default App;
