7866801014be7721a69c2eb13b230895023c4c2b
Ronaldo Santiago <ronaldo.setzer@gmail.com>
2020-09-09 03:01:14 -0300

    feat: Setup Redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
+import { Provider } from 'redux';
 
 import GlobalStyles from '@/styles/global';
 import Todo from '@/pages/Todo';
 
+import store from './store';
+
 function App() {
   return (
-    <>
+    <Provider store={store}>
       <GlobalStyles />
       <Todo />
-    </>
+    </Provider>
   );
 }
 
---
+++ src/store/modules/rootReducer.js
@@ -0,0 +1,7 @@
+import { combineReduers } from 'redux';
+
+function reduxExample(state = [], action) {
+  return state;
+}
+
+export default combineReduers({ reduxExample });
